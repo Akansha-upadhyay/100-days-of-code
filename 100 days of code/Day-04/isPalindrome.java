e820c063b24439011a62961758a42d3b90814028
Akansha Upadhyay <akanshaupadhyay@Akanshas-Air.lan>
2026-06-05 22:35:03 +0530

    Solved Roman to Integer problem

--- 100 days of code/Day-04/isPalindrome.java
+++ 100 days of code/Day-04/isPalindrome.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
@@ -25,3 +26,32 @@ class Solution {
         return true;
     }
 }
+=======
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+          
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+>>>>>>> 76dbd46 (Solved Roman to Integer problem)
