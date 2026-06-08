c1ba5ecfbe33da8612a6a3e3f6148eadda5c2aa0
Akansha Upadhyay <akanshaupadhyay@Akanshas-Air.lan>
2026-06-08 21:50:14 +0530

    Day 7: Solved Group Anagrams

---
+++ 100 days of code/Day-07/removeElements.java
@@ -0,0 +1,42 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class removeElements {
+
+    public static ListNode remove(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode curr = dummy;
+
+        while (curr.next != null) {
+            if (curr.next.val == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(3);
+
+        head = remove(head, 6);
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
