c48e981e3ff055333955b1931d434d640348a02a
Akansha Upadhyay <akanshaupadhyay@Akanshas-Air.lan>
2026-06-11 23:21:44 +0530

    Day 11 - Merge Intervals

--- 100 days of code/Day-09/maxprofit.java
+++ 100 days of code/Day-09/maxprofit.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Solution {
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
@@ -14,3 +15,21 @@ class Solution {
         return maxProfit;
     }
 }
+=======
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+}
+>>>>>>> eab3f6b (Day 11 - Merge Intervals)
