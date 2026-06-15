6ea97b9737c62b2d9fb7fc1dbeb7a78123cc15a7
Akansha Upadhyay <akanshaupadhyay@Akanshas-Air.lan>
2026-06-15 22:37:05 +0530

    Day 14: Solve Longest Palindromic Substring using Expand Around Center

--- 100 days of code/Day-13/reverseList.java
+++ 100 days of code/Day-13/reverseList.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Solution {
     public ListNode reverseList(ListNode head) {
 
@@ -15,3 +16,22 @@ class Solution {
         return prev; // new head
     }
 }
+=======
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            ListNode nextTemp = curr.next; // save next node
+            curr.next = prev;              // reverse pointer
+            prev = curr;                   // move prev forward
+            curr = nextTemp;               // move curr forward
+        }
+
+        return prev; // new head
+    }
+}
+>>>>>>> 64be3a6 (Day 14: Solve Longest Palindromic Substring using Expand Around Center)
