293a320c335be88a87f13f71980651acc1beec4a
Akansha Upadhyay <upadhyayakansha79@gmail.com>
2026-06-15 22:39:09 +0530

    Update reverseList.java

--- 100 days of code/Day-13/reverseList.java
+++ 100 days of code/Day-13/reverseList.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution {
     public ListNode reverseList(ListNode head) {
 
@@ -16,22 +15,4 @@ class Solution {
         return prev; // new head
     }
 }
-=======
-class Solution {
-    public ListNode reverseList(ListNode head) {
 
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-
-            ListNode nextTemp = curr.next; // save next node
-            curr.next = prev;              // reverse pointer
-            prev = curr;                   // move prev forward
-            curr = nextTemp;               // move curr forward
-        }
-
-        return prev; // new head
-    }
-}
->>>>>>> 64be3a6 (Day 14: Solve Longest Palindromic Substring using Expand Around Center)
